588fa8099acc21719184120ee97382176b7c4666
laurenarthur <arthur_lauren@columbusstate.edu>
2020-11-02 15:32:00 +0000

    Added comments to index

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+//express
 var express = require('express');
+
+//fetch
 var bodyParser = require('body-parser');
+
+//call express
 var app = express();
+
+//port information
 const port = process.env.PORT || 3000;
+
+//ue EJS for templates 
 app.set('view engine','ejs');
+
+//get home page
 app.get('/',function(req,res){
     res.send('Hello, this will be our project.')
 });
