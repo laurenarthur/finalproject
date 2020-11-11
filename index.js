f0028259ada65dc1b4fe4e58bcfd9a9bd390cbb9
kylemartinolich <kylemartinolich@gmail.com>
2020-11-11 03:36:53 +0000

    Kyle movie fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //express
 var express = require('express');
 
-//fetch
+//parser
 var bodyParser = require('body-parser');
 
-//request
-var request = require('request');
+//fetch
+var fetch = require('node-fetch');
 
 //call express
 var app = express();
@@ -32,21 +32,22 @@ app.get('/contact',function(req,res){
 //movie api page
 app.get('/Movie', function(req,res){
     //var result = null;
-    request('https://api.themoviedb.org/3/search/movie?api_key=2cb9d256f4796cfd3b7c89a3324b4356&language=en-US&query=big&page=1&include_adult=false', function (error, response, body) {
-    call = JSON.parse(body);
-    var result = call;
-    res.render('Movie', {result:result});
-});
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=2cb9d256f4796cfd3b7c89a3324b4356&language=en-US&query=big&page=1&include_adult=false')
+    .then(res => res.json())
+    .then(result => {
+        res.render('nasa',{result:result});
+    });
 });
 
 app.get('/searchMovie', function(req,res){
     //var search = req.body.movieSearch;
-    request('https://api.themoviedb.org/3/search/movie?api_key=2cb9d256f4796cfd3b7c89a3324b4356&language=en-US&query=big&page=1&include_adult=false', function (error, response, body) {
-    call = JSON.parse(body);
-    var result = call;
-    res.render('Movie', {result:result});
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=2cb9d256f4796cfd3b7c89a3324b4356&language=en-US&query=big&page=1&include_adult=false')
+    .then(res => res.json())
+    .then(result => {
+        res.render('nasa',{result:result});
+    });
 });
-});
+
 //tv-show api page
 app.get('/TV', function(req,res){
     //need to fetch the query that they searched 
