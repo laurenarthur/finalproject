cd8336e8a44555f564ab1e8f4c4062027f6b3023
kylemartinolich <kylemartinolich@gmail.com>
2020-11-11 03:22:08 +0000

    Kyle movie fix

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ app.get('/Movie', function(req,res){
     res.render('Movie');
 });
 
-//app.get('/searchMovie', function(req,res){
+app.get('/searchMovie', function(req,res){
     //var search = req.body.movieSearch;
    // request('https://api.themoviedb.org/3/search/movie?api_key=2cb9d256f4796cfd3b7c89a3324b4356&language=en-US&query=big&page=1&include_adult=false', function (error, response, body) {
    // call = JSON.parse(body);
  //   var result = call;
-//    res.render('Movie', {result:result});
+   res.render('Movie');
 //});
-//});
+});
 //tv-show api page
 app.get('/TV', function(req,res){
     //need to fetch the query that they searched 
